4c5ae4562520e9d4a699051f95c8862e2018163d
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2022-09-02 15:42:34 +0800

    Initial tutorial support.

--- scenes/MainMenuManager.gd
+++ scenes/MainMenuManager.gd
@@ -2,10 +2,11 @@ extends Control
 
 
 func _ready():
-	get_node("Control/PuzzCellButton").connect("pressed", self, "GotoScene", ["res://scenes/Main.tscn"])
-	get_node("Control/MicroSimButton").connect("pressed", self, "GotoScene", ["res://scenes/MicroSim/MicroSimSub1.tscn"])
-	get_node("Control/ReproDefendersButton").connect("pressed", self, "GotoScene", ["res://scenes/ReproDefender/ReproDefenderMain.tscn"])
-	get_node("Control/PredictTermsButton").connect("pressed", self, "GotoScene", ["res://scenes/GuessingGame/Game1.tscn"])
+	get_node("Control/PuzzCellButton").connect("pressed", self, "GotoScene", ["res://scenes/Tutorials/TutPuzz.tscn"])
+	get_node("Control/MicroSimButton").connect("pressed", self, "GotoScene", ["res://scenes/Tutorials/MicroSim.tscn"])
+	get_node("Control/ReproDefendersButton").connect("pressed", self, "GotoScene", ["res://scenes/Tutorials/TutReprodefenders.tscn"])
+	get_node("Control/PredictTermsButton").connect("pressed", self, "GotoScene", ["res://scenes/Tutorials/TutPredicterms.tscn"])
+	get_node("Control/HelpButton").connect("pressed", self, "GotoScene", ["res://scenes/Tutorials/Tut1.tscn"])
 	#get_node("Button3").connect("pressed", self, "GotoScene", ["res://scenes/PlantCell.tscn"])
 	#get_node("Button4").connect("pressed", self, "GotoScene", ["res://scenes/PlantCell.tscn"])
 
@@ -13,3 +14,4 @@ func GotoScene(scene):
 	$Click.play()
 	$"/root/Globals".total_score = 0
 	$"/root/Globals".goto_scene(scene)
+	
