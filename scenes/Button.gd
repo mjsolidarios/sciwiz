2b26da73a4b2c07ed48e71c85fbe7e6984cb473c
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2022-09-08 01:02:03 +0800

    Bug fixes

--- scenes/Button.gd
+++ scenes/Button.gd
@@ -1,9 +1,6 @@
-extends TextureButton
+extends Button
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var path = ""
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,10 +8,7 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
-func _on_BackButton_pressed():
+func _on_Button_pressed():
 	$Click.play()
+	$"/root/Globals".goto_scene(path)
+	$"/root/Globals".ans_score = 0
--- scenes/Globals.gd
+++ scenes/Globals.gd
@@ -30,7 +30,7 @@ var ans_score = 0
 var active_index1_for_guessing_game = 0
 var active_index2_for_guessing_game = 0
 var max_index_for_guessing_game = 0
-var guessing_game_score = 5
+var guessing_game_score = 0
 var guessing_game_life = 3
 
 var guessing_game_question_pool = [2,3,4,5,6,7,8,9,10,11,12,13,14]
