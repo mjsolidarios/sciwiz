4a3fdf09a8cdf93350c99f7ad126c254d3f278ab
mjsolidarios <mjsolidarios@wvsu.edu.ph>
2022-10-19 18:53:07 +0800

    Saved changes

--- scenes/Globals.gd
+++ scenes/Globals.gd
@@ -33,14 +33,15 @@ var max_index_for_guessing_game = 0
 var guessing_game_score = 0
 var guessing_game_life = 3
 
-var guessing_game_question_pool = [2,3,4,5,6,7,8,9,10,11,12,13,14]
+var guessing_game_question_pool = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
 var current_question_pool_index = 1
 
 
 func _ready():
 	randomize()
-	guessing_game_question_pool.shuffle()
+	# guessing_game_question_pool.shuffle()
+	# print("Question Sequence: "+str(guessing_game_question_pool))
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
 
